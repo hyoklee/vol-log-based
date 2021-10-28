c43b062ad75bf2da770dc6515f23c1102219d3eb
H. Joe Lee <hyoklee@hdfgroup.org>
2021-10-28 16:49:10 -0500

    OESS-205: Make MPI-3.0 compatible.

--- src/H5VL_logi_nb.cpp
+++ src/H5VL_logi_nb.cpp
@@ -487,7 +487,7 @@ herr_t H5VL_log_nb_flush_write_reqs (void *file, hid_t dxplid) {
 			fsize_local += fp->wreqs[i]->rsize;
 		}
 
-		mpierr = MPI_Type_hindexed (cnt, mlens, moffs, MPI_BYTE, &mtype);
+		mpierr = MPI_Type_create_hindexed (cnt, mlens, moffs, MPI_BYTE, &mtype);
 		CHECK_MPIERR
 		mpierr = MPI_Type_commit (&mtype);
 		CHECK_MPIERR
